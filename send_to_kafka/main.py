c5c5513e2091c0948fba39dddf22645876d570e2
Kris Jenkins <krisajenkins@gmail.com>
2024-04-29 09:12:07 +0100

    Committing `send_to_kafka`, as recorded.

---
+++ send_to_kafka/main.py
@@ -0,0 +1,42 @@
+import requests
+import time
+import json
+import logging
+from quixstreams import Application
+
+
+def get_weather():
+    response = requests.get(
+        "https://api.open-meteo.com/v1/forecast",
+        params={
+            "latitude": 51.5,
+            "longitude": -0.11,
+            "current": "temperature_2m",
+        },
+    )
+
+    return response.json()
+
+
+def main():
+    app = Application(
+        broker_address="localhost:9092",
+        loglevel="DEBUG",
+    )
+
+    with app.get_producer() as producer:
+        while True:
+            weather = get_weather()
+            logging.debug("Got weather: %s", weather)
+            producer.produce(
+                topic="weather_data_demo",
+                key="London",
+                value=json.dumps(weather),
+            )
+            logging.info("Produced. Sleeping...")
+            time.sleep(10)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level="DEBUG")
+    main()
